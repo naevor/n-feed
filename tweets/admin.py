68582f2f063bd47edfadde67885945272b4d8444
t3tsuvastr <doshankadyr2007@gmail.com>
2026-04-28 19:22:36 +0500

    refactor(admin): replace TagAdmin with CommentAdmin

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
-from .models import Tweet, Tag
+from .models import Comment, Tweet
 
 @admin.register(Tweet)
 class TweetAdmin(admin.ModelAdmin):
-    list_display = ('title', 'user', 'created_at')
-    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('user', 'content', 'created_at')
+    search_fields = ('content', 'user__username')
+    list_filter = ('created_at',)
 
-@admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
-    list_display = ('name' , 'created_at')
-    
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'tweet', 'created_at')
+    search_fields = ('content', 'user__username', 'tweet__content')
