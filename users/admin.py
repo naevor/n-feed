6867059cb04961186ab7b314f1fe2b1c308422fd
t3tsuvastr <doshankadyr2007@gmail.com>
2026-04-28 19:22:36 +0500

    feat(admin): register CustomUser with profile fields

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Profile', {'fields': ('bio', 'avatar', 'following')}),
+    )
+    list_display = ('username', 'email', 'is_staff', 'is_active')
