7dccd79417a1cd9a2fb49d79cb4044150ba464ed
t3tsuvastr <doshankadyr2007@gmail.com>
2026-04-28 20:19:24 +0500

    feat(tweets): add collision-safe slug generation with uuid fallback

--- tweets/models.py
+++ tweets/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
@@ -8,7 +10,7 @@ class Tweet(models.Model):
     media = models.FileField(upload_to='tweet_media/', blank=True, null=True) 
     likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='liked_tweets', blank=True)
     retweet = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='retweets')
-    slug = models.SlugField(unique=True, blank=True) 
+    slug = models.SlugField(max_length=120, unique=True, blank=True) 
     created_at = models.DateTimeField(auto_now_add=True)
     bookmarks = models.ManyToManyField(
         settings.AUTH_USER_MODEL, 
@@ -21,7 +23,14 @@ class Tweet(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(f"{self.user.username}-{self.content[:50]}")
+            base_slug = slugify(f"{self.user.username}-{self.content[:50]}") or f"tweet-{uuid.uuid4().hex[:12]}"
+            slug = base_slug[:110]
+            counter = 1
+            while Tweet.objects.filter(slug=slug).exclude(pk=self.pk).exists():
+                suffix = f"-{counter}-{uuid.uuid4().hex[:6]}"
+                slug = f"{base_slug[:120 - len(suffix)]}{suffix}"
+                counter += 1
+            self.slug = slug
         super().save(*args, **kwargs)
 
     def __str__(self):
