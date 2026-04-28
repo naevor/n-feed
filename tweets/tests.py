8f6a3ddf4638a4655c0b844aa25bf742a25120cf
t3tsuvastr <doshankadyr2007@gmail.com>
2026-04-28 20:19:25 +0500

    test(tweets): add tests for search, like, delete, slug, anonymous API

--- tweets/tests.py
+++ tweets/tests.py
@@ -1,3 +1,52 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Tweet
+
+
+class TweetViewTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='author',
+            password='testpass123',
+        )
+        self.tweet = Tweet.objects.create(user=self.user, content='hello searchable world')
+
+    def test_search_filters_tweets(self):
+        Tweet.objects.create(user=self.user, content='something else')
+
+        response = self.client.get(reverse('tweets:all_tweets'), {'q': 'searchable'})
+
+        self.assertContains(response, 'hello searchable world')
+        self.assertNotContains(response, 'something else')
+
+    def test_like_requires_post(self):
+        self.client.login(username='author', password='testpass123')
+
+        response = self.client.get(reverse('tweets:like_tweet', args=[self.tweet.id]))
+
+        self.assertEqual(response.status_code, 405)
+        self.assertEqual(self.tweet.likes.count(), 0)
+
+    def test_delete_requires_post(self):
+        self.client.login(username='author', password='testpass123')
+
+        response = self.client.get(reverse('tweets:delete_tweet', args=[self.tweet.id]))
+
+        self.assertEqual(response.status_code, 405)
+        self.assertTrue(Tweet.objects.filter(id=self.tweet.id).exists())
+
+    def test_cyrillic_content_gets_non_empty_unique_slug(self):
+        tweet = Tweet.objects.create(user=self.user, content='привет мир')
+
+        self.assertTrue(tweet.slug)
+
+    def test_anonymous_api_post_is_denied(self):
+        response = self.client.post(
+            reverse('tweets:tweet-list-create'),
+            {'content': 'anonymous api post'},
+        )
+
+        self.assertEqual(response.status_code, 403)
+        self.assertFalse(Tweet.objects.filter(content='anonymous api post').exists())
