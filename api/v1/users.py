177e66bbc5309e1d06ef1874396d5118e286bc2f
t3tsuvastr <doshankadyr2007@gmail.com>
2026-06-07 22:27:41 +0500

    feat(api): add current user profile endpoint

--- api/v1/users.py
+++ api/v1/users.py
@@ -7,7 +7,12 @@ from rest_framework.response import Response
 
 from users import services
 from users.selectors import suggested_users
-from users.serializers import UserDetailSerializer, UserMinSerializer
+from users.serializers import (
+    UserDetailSerializer,
+    UserMinSerializer,
+    UserPrivateSerializer,
+    UserUpdateSerializer,
+)
 
 User = get_user_model()
 
@@ -21,6 +26,23 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
 
     def get_queryset(self):
         return User.objects.prefetch_related("followers", "following").all()
+
+    @extend_schema(methods=["GET"], responses=UserPrivateSerializer)
+    @extend_schema(methods=["PATCH"], request=UserUpdateSerializer, responses=UserPrivateSerializer)
+    @action(detail=False, methods=["get", "patch"], permission_classes=[IsAuthenticated])
+    def me(self, request):
+        if request.method == "PATCH":
+            serializer = UserUpdateSerializer(
+                request.user,
+                data=request.data,
+                partial=True,
+                context=self.get_serializer_context(),
+            )
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+
+        user = User.objects.prefetch_related("followers", "following").get(pk=request.user.pk)
+        return Response(UserPrivateSerializer(user, context=self.get_serializer_context()).data)
 
     @extend_schema(
         responses=inline_serializer(
